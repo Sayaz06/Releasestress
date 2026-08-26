09f07d199000c71eb5d713e2525969a635484518
Sayaz06 <adha86614@gmail.com>
2026-08-26 11:55:02 +0800

    Increment version number to v41
    Updated version number to trigger cache refresh.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Tukar VERSI setiap kali update kod supaya
 // cache lama dibuang dan app di-refresh automatik.
 
-const VERSI = 'v40'; // ← TUKAR NOMBOR INI SETIAP KALI UPDATE
+const VERSI = 'v41'; // ← TUKAR NOMBOR INI SETIAP KALI UPDATE
 const CACHE_NAME = `istigfar-${VERSI}`;
 
 const ASSETS = [
