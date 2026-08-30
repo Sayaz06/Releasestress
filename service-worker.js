25d06b5fb5c2762bf026c3339b62e30549949ba5
Sayaz06 <adha86614@gmail.com>
2026-08-30 21:22:51 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Tukar VERSI setiap kali update kod supaya
 // cache lama dibuang dan app di-refresh automatik.
 
-const VERSI = 'v80'; // ← TUKAR NOMBOR INI SETIAP KALI UPDATE
+const VERSI = 'v81'; // ← TUKAR NOMBOR INI SETIAP KALI UPDATE
 const CACHE_NAME = `istigfar-${VERSI}`;
 
 const ASSETS = [
