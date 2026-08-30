29bbae756251f9294738af89a2b803bf408fa99c
Sayaz06 <adha86614@gmail.com>
2026-08-30 11:32:20 +0800

    Increment version number to v67
    Updated version number to trigger cache refresh.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Tukar VERSI setiap kali update kod supaya
 // cache lama dibuang dan app di-refresh automatik.
 
-const VERSI = 'v66'; // ← TUKAR NOMBOR INI SETIAP KALI UPDATE
+const VERSI = 'v67'; // ← TUKAR NOMBOR INI SETIAP KALI UPDATE
 const CACHE_NAME = `istigfar-${VERSI}`;
 
 const ASSETS = [
